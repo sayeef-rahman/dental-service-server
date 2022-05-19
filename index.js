2e3c76dbc711054cd83a5b0218c44210746473f8
sayeef-rahman <sayeef.cse@gmail.com>
2022-05-19 15:52:44 +0600

    Database working fine

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello From Doctor Portal!')
+  res.send('Hello From Doctor Portal Uncle!')
 })
 
 app.listen(port, () => {
